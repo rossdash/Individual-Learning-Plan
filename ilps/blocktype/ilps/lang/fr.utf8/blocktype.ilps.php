277ad9d74bdc0f37dd2ca7bb5e2e8042a76a269a
Ross Dash <Rosser_D@me.com>
2012-04-23 10:22:54 +0100

    Added French translation, thanks to Dominique-Alain JAN aka dajan

---
+++ ilps/blocktype/ilps/lang/fr.utf8/blocktype.ilps.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage artefact-ilps
+ * @author     Ross Dash
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2012 Ross Dash
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Votre PPF';
+$string['description'] = 'Afficher la liste de vos Plans personnels de formation';
+$string['defaulttitledescription'] = 'Si la rubrique est laissée vide, le titre du PPF sera utilisé par défaut.';
+$string['newerilps'] = 'Nouveau PPF';
+$string['noilpsaddone'] = 'Pas encore de PFP. %sAjoutez-en un %s !';
+$string['olderilps'] = 'Anciens PPF';
+$string['ilpstoshow'] = 'PPF à afficher';
+?>
---
+++ ilps/lang/fr.utf8/artefact.ilps.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2008 Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage artefact-ilps
+ * @author     Ross Dash
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2012 Ross Dash
+ *
+ */
+
+defined('INTERNAL') || die();
+
+/* ilps */
+$string['description'] = 'Déscription';
+$string['deleteilpconfirm'] = 'Voulez-vous réellement supprimer ce PPF ? En supprimant le PPF, vous supprimez aussi toutes les unités de formation qu\'il contient.';
+$string['deleteilp'] = 'Supprimer PPF';
+$string['deletethisilp'] = 'Supprimer PPF : « %s »';
+$string['editilp'] = 'Modifier PPF';
+$string['editingilp'] = 'Modifiication du PPF';
+$string['manageunits'] = 'Gérer les unités de formation';
+$string['myilps'] = 'Plan personnel de formation';
+$string['newilp'] = 'Nouveau PPF';
+$string['noilpsaddone'] = 'Pas encore de PPF. %sAjoutez-en un%s !';
+$string['noilps'] = 'Pas de PPF à afficher';
+$string['ilp'] = 'PPF';
+$string['ilps'] = 'PPF';
+$string['ilpdeletedsuccessfully'] = 'PPF supprimé avec succès.';
+$string['ilpnotdeletedsuccessfully'] = 'Une erreur s\'est produire lors de la suppression du PFP.';
+$string['ilpnotsavedsuccessfully'] = 'Une erreur s\'est produire l\’or de l\'envoi du formulaire. Contrôlez les rubriques marquées et essayez à nouveau.';
+$string['ilpsavedsuccessfully'] = 'PPF enregistré avec succès.';
+$string['ilpsunits'] = 'Unités de formation du PPF « %s »';
+$string['ilpsunitsdesc'] = 'Ajouter ci-dessous des unités de formation en utilisant ou cliquez sur le bouton à droite button pour commencer à construire votre PPF.';
+$string['saveilp'] = 'Enregistrer le PPF';
+$string['title'] = 'Titre';
+$string['titledesc'] = 'Le titre sera utilisé pour afficher cette unité de formation dans ce bloc PPF.';
+$string['status'] = 'État';
+
+/* units */
+$string['allunits'] = 'Toutes les unités de formation';
+$string['points'] = 'crédits';
+$string['acquiredpoints'] = 'Crédits acquis';
+$string['remainingpoints'] = 'Crédits manquants';
+$string['totalpoints'] = 'Total des crédits';
+$string['targetcompletion'] = 'Date de fin estimée';
+$string['datecompleted'] = 'Terminé le';
+$string['pointsdesc'] = 'La valeur en crédits de cette unité de formation.';
+$string['deleteunitconfirm'] = 'Voulez-vous supprimer cette unité de formation ?';
+$string['deleteunit'] = 'Suprimer l\'unité de formation';
+$string['deletethisunit'] = 'Supprimer l\'unité de formation : « %s »';
+$string['editunit'] = 'Modifier l\'unité de formation';
+$string['editingunit'] = 'Modification de l\'unité de formation';
+$string['myunits'] = 'Unités de formation';
+$string['newunit'] = 'Nouvelle unité de formation';
+$string['nounits'] = 'Aucune unité de formation à afficher.';
+$string['nounitsaddone'] = 'Pas encore d\'unité de formation. %Ajoutez-en une%s !';
+$string['saveunit'] = 'Enregistrer l\'unité de formation';
+$string['unit'] = 'Unité de formation';
+$string['units'] = 'Unités de formation';
+$string['unitdeletedsuccessfully'] = 'unité de formation supprimée avec succès.';
+$string['unitsavedsuccessfully'] = 'unité de formation enregistrée avec succès.';
+$string['status'] = 'État';
+$string['statusdesc'] = 'L\'état de complétude de l\'unité de formation (par ex. : terminée, en cours, à venir).';
+?>
